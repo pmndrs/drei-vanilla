9b00df224cb2f03a63741fbc955fedb17b712386
vis-prime <vishals1030@gmail.com>
2023-04-24 22:30:48 +0530

    chore: meshTransmissionMaterial storybook cleanup

--- .storybook/stories/MeshTransmissionMaterial.stories.ts
+++ .storybook/stories/MeshTransmissionMaterial.stories.ts
@@ -53,6 +53,9 @@ export const MTMStory = async () => {
   setupMeshTransmissionMaterial()
 }
 
+/**
+ * Add scene.environment and groundProjected skybox
+ */
 const setupEnvironment = () => {
   const exrLoader = new EXRLoader()
 
@@ -68,14 +71,18 @@ const setupEnvironment = () => {
   })
 }
 
+/**
+ * Add a torus which uses mesh transmission material
+ */
 function setupMeshTransmissionMaterial() {
   const discardMaterial = new MeshDiscardMaterial()
   const meshTransmissionMaterial = new MeshTransmissionMaterial(6, false)
 
   const geometry = new THREE.TorusKnotGeometry(3, 1, 100, 32).translate(0, 6, 0)
-  const model = new THREE.Mesh(geometry, new THREE.MeshStandardMaterial())
+  const model = new THREE.Mesh(geometry, meshTransmissionMaterial)
 
-  const transmissionMeshes = []
+  const transmissionMeshes: THREE.Mesh[] = []
+
   model.traverse((child) => {
     if (child instanceof THREE.Mesh) {
       child.castShadow = true
@@ -85,8 +92,6 @@ function setupMeshTransmissionMaterial() {
     }
   })
   scene.add(model)
-
-  addTransmissionGui(gui, meshTransmissionMaterial)
 
   const fboBack = new THREE.WebGLRenderTarget(512, 512, {
     minFilter: THREE.LinearFilter,
@@ -102,8 +107,8 @@ function setupMeshTransmissionMaterial() {
     type: THREE.HalfFloatType,
   })
 
-  const mtm = meshTransmissionMaterial
-  mtm.buffer = fboMain.texture
+  meshTransmissionMaterial.buffer = fboMain.texture
+
   let oldBg: THREE.Color | THREE.Texture | null
   let oldTone: THREE.ToneMapping
   let oldSide: THREE.Side
@@ -113,48 +118,48 @@ function setupMeshTransmissionMaterial() {
     camera,
   }
 
+  addTransmissionGui(gui, meshTransmissionMaterial)
+
   // runs on every frame
   animateLoop((time: number) => {
-    mtm.time = time * 0.001
+    meshTransmissionMaterial.time = time * 0.001
 
     for (const mesh of transmissionMeshes) {
-      const parent = mesh
-
-      if (mtm.buffer === fboMain.texture) {
+      if (meshTransmissionMaterial.buffer === fboMain.texture) {
         // Save defaults
         oldTone = state.gl.toneMapping
         oldBg = state.scene.background
-        oldSide = parent.material.side
+        oldSide = mesh.material.side
 
         // Switch off tonemapping lest it double tone maps
         // Save the current background and set the HDR as the new BG
         // Use discardMaterial, the parent will be invisible, but it's shadows will still be cast
         state.gl.toneMapping = THREE.NoToneMapping
-        parent.material = discardMaterial
+        mesh.material = discardMaterial
 
         if (mtmParams.backside) {
           // Render into the backside buffer
           state.gl.setRenderTarget(fboBack)
           state.gl.render(state.scene, state.camera)
           // And now prepare the material for the main render using the backside buffer
-          parent.material = mtm
-          parent.material.buffer = fboBack.texture
-          parent.material.thickness = mtmParams.backsideThickness
-          parent.material.side = THREE.BackSide
+          mesh.material = meshTransmissionMaterial
+          mesh.material.buffer = fboBack.texture
+          mesh.material.thickness = mtmParams.backsideThickness
+          mesh.material.side = THREE.BackSide
         }
 
         // Render into the main buffer
         state.gl.setRenderTarget(fboMain)
         state.gl.render(state.scene, state.camera)
 
-        parent.material.thickness = mtmParams.thickness
-        parent.material.side = oldSide
-        parent.material.buffer = fboMain.texture
+        mesh.material.thickness = mtmParams.thickness
+        mesh.material.side = oldSide
+        mesh.material.buffer = fboMain.texture
 
         // Set old state back
         state.scene.background = oldBg
         state.gl.setRenderTarget(null)
-        parent.material = mtm
+        mesh.material = meshTransmissionMaterial
         state.gl.toneMapping = oldTone
       }
     }
@@ -182,8 +187,8 @@ function addTransmissionGui(gui: GUI, mat: MeshTransmissionMaterial) {
   fol.add(mat, 'distortion', 0, 10)
   fol.add(mat, 'temporalDistortion', 0, 1)
   fol.add(mtmParams, 'backside')
-  fol.add(mtmParams, 'thickness', 0, 2)
-  fol.add(mtmParams, 'backsideThickness', 0, 2)
+  fol.add(mtmParams, 'thickness', 0, 4)
+  fol.add(mtmParams, 'backsideThickness', 0, 4)
 }
 
 MTMStory.storyName = 'TorusKnot'
