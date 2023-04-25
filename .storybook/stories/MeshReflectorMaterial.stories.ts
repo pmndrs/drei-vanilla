318ce8a7145277b1b62e92efc56aafc8f9ebe8b2
vis-prime <vishals1030@gmail.com>
2023-04-25 22:58:24 +0530

    chore: meshReflector  story cleanup

--- .storybook/stories/MeshReflectorMaterial.stories.ts
+++ .storybook/stories/MeshReflectorMaterial.stories.ts
@@ -11,24 +11,46 @@ export default {
   title: 'Shaders/MeshReflectorMaterial',
 } as Meta // TODO: this should be `satisfies Meta` but commit hooks lag behind TS
 
-const MRMParams = {
+const TargetParams = {
   resolution: 1024,
   blurX: 1024,
   blurY: 1024,
   depthScale: 1,
 }
 
+const MRMParams = {
+  mixBlur: 1,
+  mixStrength: 5,
+  minDepthThreshold: 0,
+  maxDepthThreshold: 1,
+  depthToBlurRatioBias: 0.25,
+  mirror: 0,
+  distortion: 0.25,
+  mixContrast: 1,
+  reflectorOffset: 0,
+}
+
 const params = {
+  color: new THREE.Color('#151515'),
   useRoughnessMap: false,
+  roughness: 1,
+  metalness: 0.6,
   useDistortionMap: false,
   useNormalMap: false,
   normalScale: 1,
   repeat: 5,
 }
 
-let gui: GUI
-let scene: THREE.Scene, camera: THREE.Camera, renderer: THREE.WebGLRenderer, animateLoop
+let gui: GUI, materialGuiFolder: GUI
+let scene: THREE.Scene,
+  camera: THREE.PerspectiveCamera,
+  renderer: THREE.WebGLRenderer,
+  animateLoop: (arg0: (time: number) => void) => void,
+  roughnessTexture: THREE.Texture,
+  normalTexture: THREE.Texture,
+  meshReflectorMaterial: MeshReflectorMaterial
 const textureLoader = new THREE.TextureLoader()
+
 export const MRMStory = async () => {
   const setupResult = Setup()
   scene = setupResult.scene
@@ -65,7 +87,7 @@ const setupEnvironment = () => {
   const exrLoader = new EXRLoader()
 
   // exr from polyhaven.com
-  exrLoader.load('round_platform_1k.exr', (exrTex) => {
+  exrLoader.load('dancing_hall_1k.exr', (exrTex) => {
     exrTex.mapping = THREE.EquirectangularReflectionMapping
     scene.environment = exrTex
     // scene.background = exrTex
@@ -91,24 +113,22 @@ async function setupMeshReflectorMaterial() {
   scene.add(model)
 
   // Add Reflector
-  let mixBlur = 1,
-    mixStrength = 5,
-    minDepthThreshold = 0,
-    maxDepthThreshold = 1,
-    depthToBlurRatioBias = 0.25,
-    mirror = 0,
-    distortion = 0.25,
-    mixContrast = 1,
-    reflectorOffset = 0,
-    metalness = 0.6,
-    roughness = 1,
-    color = new THREE.Color('#151515')
+  // let mixBlur = 1,
+  //   mixStrength = 5,
+  //   minDepthThreshold = 0,
+  //   maxDepthThreshold = 1,
+  //   depthToBlurRatioBias = 0.25,
+  //   mirror = 0,
+  //   distortion = 0.25,
+  //   mixContrast = 1,
+  //   reflectorOffset = 0,
+  //   metalness = 0.6,
+  //   roughness = 1,
+  //   color = new THREE.Color('#151515')
 
   const gl = renderer
 
-  // blur = Array.isArray(blur) ? blur : [blur, blur]
-  let hasBlur = MRMParams.blurX + MRMParams.blurY > 0
-  // const materialRef = React.useRef<MeshReflectorMaterialImpl>(null!)
+  let hasBlur = TargetParams.blurX + TargetParams.blurY > 0
   const reflectorPlane = new THREE.Plane()
   const normal = new THREE.Vector3()
   const reflectorWorldPosition = new THREE.Vector3()
@@ -129,118 +149,105 @@ async function setupMeshReflectorMaterial() {
       encoding: gl.outputEncoding,
       type: THREE.HalfFloatType,
     }
-    const fbo1 = new THREE.WebGLRenderTarget(MRMParams.resolution, MRMParams.resolution, parameters)
+    const fbo1 = new THREE.WebGLRenderTarget(TargetParams.resolution, TargetParams.resolution, parameters)
     fbo1.depthBuffer = true
-    fbo1.depthTexture = new THREE.DepthTexture(MRMParams.resolution, MRMParams.resolution)
+    fbo1.depthTexture = new THREE.DepthTexture(TargetParams.resolution, TargetParams.resolution)
     fbo1.depthTexture.format = THREE.DepthFormat
     fbo1.depthTexture.type = THREE.UnsignedShortType
-    const fbo2 = new THREE.WebGLRenderTarget(MRMParams.resolution, MRMParams.resolution, parameters)
+    const fbo2 = new THREE.WebGLRenderTarget(TargetParams.resolution, TargetParams.resolution, parameters)
+
     const blurpass = new BlurPass({
       gl,
-      resolution: MRMParams.resolution,
-      width: MRMParams.blurX,
-      height: MRMParams.blurY,
-      minDepthThreshold,
-      maxDepthThreshold,
-      depthScale: MRMParams.depthScale,
-      depthToBlurRatioBias,
+      resolution: TargetParams.resolution,
+      width: TargetParams.blurX,
+      height: TargetParams.blurY,
+      minDepthThreshold: MRMParams.minDepthThreshold,
+      maxDepthThreshold: MRMParams.maxDepthThreshold,
+      depthScale: TargetParams.depthScale,
+      depthToBlurRatioBias: MRMParams.depthToBlurRatioBias,
     })
 
     const reflectorProps = {
-      mirror,
+      mirror: MRMParams.mirror,
       textureMatrix,
-      mixBlur,
+      mixBlur: MRMParams.mixBlur,
       tDiffuse: fbo1.texture,
       tDepth: fbo1.depthTexture,
       tDiffuseBlur: fbo2.texture,
       hasBlur,
-      mixStrength,
-      minDepthThreshold,
-      maxDepthThreshold,
-      depthScale: MRMParams.depthScale,
-      depthToBlurRatioBias,
-      distortion,
-      mixContrast,
-      metalness,
-      roughness,
-      color,
+      mixStrength: MRMParams.mixStrength,
+      minDepthThreshold: MRMParams.minDepthThreshold,
+      maxDepthThreshold: MRMParams.maxDepthThreshold,
+      depthScale: TargetParams.depthScale,
+      depthToBlurRatioBias: MRMParams.depthToBlurRatioBias,
+      distortion: MRMParams.distortion,
+      mixContrast: MRMParams.mixContrast,
+      metalness: params.metalness,
+      roughness: params.roughness,
+      color: params.color,
     }
 
     const defines = {
       'defines-USE_BLUR': hasBlur ? '' : undefined,
-      'defines-USE_DEPTH': MRMParams.depthScale > 0 ? '' : undefined,
+      'defines-USE_DEPTH': TargetParams.depthScale > 0 ? '' : undefined,
       'defines-USE_DISTORTION': params.useDistortionMap ? '' : undefined,
     }
-    console.log({ fbo1, fbo2, blurpass, reflectorProps, defines })
+
     return [fbo1, fbo2, blurpass, reflectorProps, defines]
   }
 
   let [fbo1, fbo2, blurpass, reflectorProps, defines] = getTargets()
 
-  // changing some properties requires creation new instance of mesh reflector material
-  function updateTargets() {
+  // changing some properties requires creation new instance of mesh reflector materials
+  function recreateTargets() {
+    console.log('Making new MeshReflectorMaterial instance')
+    // dispose old targets
     fbo1.dispose()
     fbo2.dispose()
     blurpass.renderTargetA.dispose()
     blurpass.renderTargetB.dispose()
     blurpass.convolutionMaterial.dispose()
 
-    hasBlur = MRMParams.blurX + MRMParams.blurY > 0
+    hasBlur = TargetParams.blurX + TargetParams.blurY > 0
+    // update references
     ;[fbo1, fbo2, blurpass, reflectorProps, defines] = getTargets()
 
-    // remake the material
-    material.dispose() //dispose old mat
-    material = new MeshReflectorMaterial(reflectorProps)
-    material.defines.USE_BLUR = defines['defines-USE_BLUR']
-    material.defines.USE_DEPTH = defines['defines-USE_DEPTH']
-    material.defines.USE_DISTORTION = defines['defines-USE_DISTORTION']
+    // make a new instance of the material
+    meshReflectorMaterial.dispose() //dispose old mat
+    meshReflectorMaterial = new MeshReflectorMaterial(reflectorProps)
+    meshReflectorMaterial.defines.USE_BLUR = defines['defines-USE_BLUR']
+    meshReflectorMaterial.defines.USE_DEPTH = defines['defines-USE_DEPTH']
+    meshReflectorMaterial.defines.USE_DISTORTION = defines['defines-USE_DISTORTION']
 
     updateTextures()
 
     // apply
-    reflectionMesh.material = material
+    reflectionMesh.material = meshReflectorMaterial
+    createMeshReflectorGui()
   }
 
-  function updateTextures() {
-    if (params.useRoughnessMap) {
-      material.roughnessMap = roughMap
-    } else {
-      material.roughnessMap = null
-    }
+  meshReflectorMaterial = new MeshReflectorMaterial(reflectorProps)
+  meshReflectorMaterial.defines.USE_BLUR = defines['defines-USE_BLUR']
+  meshReflectorMaterial.defines.USE_DEPTH = defines['defines-USE_DEPTH']
+  meshReflectorMaterial.defines.USE_DISTORTION = defines['defines-USE_DISTORTION']
 
-    if (params.useDistortionMap) {
-      material.distortionMap = roughMap
-    } else {
-      material.distortionMap = null
-    }
-
-    if (params.useNormalMap) {
-      material.normalMap = nrmMap
-    } else {
-      material.normalMap = null
-    }
-
-    material.needsUpdate = true
-  }
-
-  let material = new MeshReflectorMaterial(reflectorProps)
-  material.defines.USE_BLUR = defines['defines-USE_BLUR']
-  material.defines.USE_DEPTH = defines['defines-USE_DEPTH']
-  material.defines.USE_DISTORTION = defines['defines-USE_DISTORTION']
-
-  const [roughMap, nrmMap] = await Promise.all([
+  const [rough, norm] = await Promise.all([
     textureLoader.loadAsync('./rgh.jpg'),
     textureLoader.loadAsync('./paper_normal.jpg'),
   ])
-  roughMap.wrapS = THREE.RepeatWrapping
-  roughMap.wrapT = THREE.RepeatWrapping
+  roughnessTexture = rough
+  normalTexture = norm
 
-  nrmMap.wrapS = THREE.RepeatWrapping
-  nrmMap.wrapT = THREE.RepeatWrapping
-  nrmMap.repeat.set(5, 5)
-  roughMap.repeat.set(5, 5)
+  roughnessTexture.wrapS = THREE.RepeatWrapping
+  roughnessTexture.wrapT = THREE.RepeatWrapping
 
-  const reflectionMesh = new THREE.Mesh(new THREE.CircleGeometry(20, 32), material)
+  normalTexture.wrapS = THREE.RepeatWrapping
+  normalTexture.wrapT = THREE.RepeatWrapping
+
+  normalTexture.repeat.set(5, 5)
+  roughnessTexture.repeat.set(5, 5)
+
+  const reflectionMesh = new THREE.Mesh(new THREE.CircleGeometry(20, 32), meshReflectorMaterial)
   reflectionMesh.rotateX(-Math.PI / 2)
 
   reflectionMesh.name = 'reflection_catcher'
@@ -250,48 +257,24 @@ async function setupMeshReflectorMaterial() {
 
   // GUI
 
-  const matProps = gui.addFolder('Material props')
-  matProps.addColor(material, 'color')
-
-  matProps.add(material, 'metalness', 0, 1)
-
-  matProps.add(params, 'useRoughnessMap').onChange(updateTextures)
-  matProps.add(material, 'roughness', 0, 1)
-
-  matProps.add(params, 'useDistortionMap').onChange(updateTextures)
-  matProps.add(material, 'distortion', -2, 2)
-
-  matProps.add(params, 'useNormalMap').onChange(updateTextures)
-  matProps.add(params, 'normalScale', 0, 1).onChange((v: number) => {
-    material.normalScale.setScalar(v)
-  })
-
-  matProps.add(params, 'repeat', 1, 15, 1).onChange((v) => {
-    roughMap.repeat.setScalar(v)
-    nrmMap.repeat.setScalar(v)
-  })
-
-  matProps.add(material, 'mixStrength', 0, 15)
-  matProps.add(material, 'mixBlur', 0, 6)
-  matProps.add(material, 'mixContrast', 0, 5)
-
   // Changing these values creates another meshReflector instances and render targets
   const heavyActions = gui.addFolder('⚠ Heavy Actions')
-  heavyActions.onChange(updateTargets)
+  heavyActions.onChange(recreateTargets)
   heavyActions.open()
-  heavyActions.add(MRMParams, 'resolution', 128, 2048, 128).name('⚠ Resolution')
-  heavyActions.add(MRMParams, 'blurX', 16, 2048, 128).name('⚠ Blur X')
-  heavyActions.add(MRMParams, 'blurY', 16, 2048, 128).name('⚠ Blur Y')
-  heavyActions.add(MRMParams, 'depthScale', 0, 10).name('⚠ DEPTH SCALE')
+  heavyActions.add(TargetParams, 'resolution', 128, 2048, 128).name('⚠ Resolution')
+  heavyActions.add(TargetParams, 'blurX', 16, 2048, 128).name('⚠ Blur X')
+  heavyActions.add(TargetParams, 'blurY', 16, 2048, 128).name('⚠ Blur Y')
+  heavyActions.add(TargetParams, 'depthScale', 0, 4).name('⚠ DEPTH SCALE')
 
-  // call when  camera, reflectorOffset changes
+  createMeshReflectorGui()
+
   const beforeRender = () => {
     reflectorWorldPosition.setFromMatrixPosition(reflectionMesh.matrixWorld)
     cameraWorldPosition.setFromMatrixPosition(camera.matrixWorld)
     rotationMatrix.extractRotation(reflectionMesh.matrixWorld)
     normal.set(0, 0, 1)
     normal.applyMatrix4(rotationMatrix)
-    reflectorWorldPosition.addScaledVector(normal, reflectorOffset)
+    reflectorWorldPosition.addScaledVector(normal, MRMParams.reflectorOffset)
     view.subVectors(reflectorWorldPosition, cameraWorldPosition)
     // Avoid rendering when reflector is facing away
     if (view.dot(normal) > 0) return
@@ -338,9 +321,9 @@ async function setupMeshReflectorMaterial() {
 
   // runs on every frame
   animateLoop((time: number) => {
-    model.rotation.x += time * 0.00000001
-    model.rotation.y += time * 0.00000002
-    model.rotation.z += time * 0.00000003
+    model.rotation.x = time * 0.0001
+    model.rotation.y = time * 0.0002
+    model.rotation.z = time * 0.0003
 
     reflectionMesh.visible = false
     const currentXrEnabled = gl.xr.enabled
@@ -358,6 +341,94 @@ async function setupMeshReflectorMaterial() {
     gl.shadowMap.autoUpdate = currentShadowAutoUpdate
     reflectionMesh.visible = true
     gl.setRenderTarget(null)
+  })
+}
+
+/**
+ * Apply or remove textures
+ */
+function updateTextures() {
+  if (params.useRoughnessMap) {
+    meshReflectorMaterial.roughnessMap = roughnessTexture
+  } else {
+    meshReflectorMaterial.roughnessMap = null
+  }
+
+  if (params.useDistortionMap) {
+    meshReflectorMaterial.distortionMap = roughnessTexture
+  } else {
+    meshReflectorMaterial.distortionMap = null
+  }
+
+  if (params.useNormalMap) {
+    meshReflectorMaterial.normalMap = normalTexture
+  } else {
+    meshReflectorMaterial.normalMap = null
+  }
+
+  meshReflectorMaterial.needsUpdate = true
+}
+
+/**
+ * Create gui for material properties
+ */
+function createMeshReflectorGui() {
+  if (materialGuiFolder) {
+    materialGuiFolder.destroy()
+  }
+
+  const matProps = gui.addFolder('MeshReflectorMaterial id: ' + meshReflectorMaterial.id)
+
+  matProps.addColor(meshReflectorMaterial, 'color')
+
+  materialGuiFolder = matProps
+
+  matProps.add(params, 'metalness', 0, 1).onChange((v) => {
+    meshReflectorMaterial.metalness = v
+  })
+
+  matProps.add(params, 'useRoughnessMap').onChange(updateTextures)
+  matProps.add(params, 'roughness', 0, 1).onChange((v) => {
+    meshReflectorMaterial.roughness = v
+  })
+
+  matProps.add(params, 'useNormalMap').onChange(updateTextures)
+  matProps.add(params, 'normalScale', 0, 1).onChange((v: number) => {
+    meshReflectorMaterial.normalScale.setScalar(v)
+  })
+
+  matProps.add(params, 'repeat', 1, 15, 1).onChange((v: any) => {
+    roughnessTexture.repeat.setScalar(v)
+    normalTexture.repeat.setScalar(v)
+  })
+
+  matProps.add(params, 'useDistortionMap').onChange(updateTextures)
+  matProps.add(MRMParams, 'distortion', -2, 2).onChange((v) => {
+    meshReflectorMaterial.distortion = v
+  })
+  matProps.add(MRMParams, 'mixStrength', 0, 15).onChange((v) => {
+    meshReflectorMaterial.mixStrength = v
+  })
+  matProps.add(MRMParams, 'mixBlur', 0, 6).onChange((v) => {
+    meshReflectorMaterial.mixBlur = v
+  })
+  matProps.add(MRMParams, 'mixContrast', 0, 5).onChange((v) => {
+    meshReflectorMaterial.mixContrast = v
+  })
+  matProps.add(MRMParams, 'reflectorOffset', -5, 5)
+
+  matProps.add(MRMParams, 'mirror', 0, 1, 1).onChange((v) => {
+    meshReflectorMaterial.mirror = v
+  })
+  matProps.add(MRMParams, 'depthToBlurRatioBias', 0, 1).onChange((v) => {
+    meshReflectorMaterial.depthToBlurRatioBias = v
+  })
+
+  matProps.add(MRMParams, 'minDepthThreshold', 0, 10).onChange((v) => {
+    meshReflectorMaterial.minDepthThreshold = v
+  })
+  matProps.add(MRMParams, 'maxDepthThreshold', 0, 10).onChange((v) => {
+    meshReflectorMaterial.maxDepthThreshold = v
   })
 }
 
