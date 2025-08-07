bf1e48229faee0d38b07c8e53342cfd6448f4e31
VishaL <119810373+vis-prime@users.noreply.github.com>
2025-08-07 14:29:00 +0530

    fix: update three and other dependencies and fix typescript errors (#89)
    * chore: update @types/three and three dependencies to latest versions
    
    * fix: update renderer type to WebGLRenderer in pcss and Pass classes
    
    * fix: simplify FBOSettings and improve useFBO function structure
    
    * fix: cast texture properties to any for compatibility in Splat functions
    
    * fix: rename depth property to depthBuffer in NORMALPROPS to follow updated useFBO params

--- src/core/useFBO.ts
+++ src/core/useFBO.ts
@@ -1,9 +1,7 @@
 import * as THREE from 'three'
 
 type FBOSettings = {
-  /** Defines the count of MSAA samples. Can only be used with WebGL 2. Default: 0 */
-  samples?: number
-  /** If set, the scene depth will be rendered into buffer.depthTexture. Default: false */
+  /** @deprecated use `depthBuffer` instead. If set, the scene depth will be rendered into buffer.depthTexture. Default: false */
   depth?: boolean
 } & THREE.RenderTargetOptions
 
@@ -13,33 +11,23 @@ function useFBO(
   /** Height in pixels */
   height = 1024,
   /**Settings */
-  settings: FBOSettings = {
-    samples: 0,
-    depth: false,
-  }
+  settings?: FBOSettings
 ): THREE.WebGLRenderTarget {
   var _width = width
   var _height = height
-  var _settings = settings
-  var samples = _settings.samples || 0
-  var depth = _settings.depth
-  var targetSettings = Object.assign({}, _settings)
-  delete targetSettings.samples
-  delete targetSettings.depth
-  var target = new THREE.WebGLRenderTarget(
-    _width,
-    _height,
-    Object.assign(
-      {
-        minFilter: THREE.LinearFilter,
-        magFilter: THREE.LinearFilter,
-        type: THREE.HalfFloatType,
-      },
-      targetSettings
-    )
-  )
+  var _settings = settings || {}
+  const { samples = 0, depth, ...targetSettings } = _settings
 
-  if (depth) {
+  const depthBuffer = depth ?? _settings.depthBuffer // backwards compatibility for deprecated `depth` prop
+
+  var target = new THREE.WebGLRenderTarget(_width, _height, {
+    minFilter: THREE.LinearFilter,
+    magFilter: THREE.LinearFilter,
+    type: THREE.HalfFloatType,
+    ...targetSettings,
+  })
+
+  if (depthBuffer) {
     target.depthTexture = new THREE.DepthTexture(_width, _height, THREE.FloatType)
   }
 
